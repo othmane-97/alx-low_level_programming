dcaa6f9dd435b428b2533ad10445cba5ab524d2a
othmane-97 <othmane.chafi.1997@gmail.com>
2023-06-20 15:59:31 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - Entry point for the program
+ *
+ * Description: Check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+print_alphabet();
+return (0);
+}
