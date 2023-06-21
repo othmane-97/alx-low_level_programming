04b0e90344db75ac1fb1114a4da6e7956ecce3e2
othmane-97 <othmane.chafi.1997@gmail.com>
2023-06-20 18:32:11 -0700

    times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,12 +19,16 @@ if (k < 10 && j != 0)
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
-_putchar(k + '0');
+_putchar(k + '0')
+}
 else if (k >= 10)
+{
 _putchar(',');
 _putchar(' ');
 _putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
+}
+}
 _putchar('\n');
 }
 }
