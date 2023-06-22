d12274714626362d0c0c07879b88f509f228159f
othmane-97 <othmane.chafi.1997@gmail.com>
2023-06-22 12:40:52 -0700

    more functions more nested loops

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_triangle - Prints a triangle of a specified size using the character '#'.
+ * @size: The size of the triangle.
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+if (size <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+for (i = 1; i <= size; i++)
+{
+for (j = i; j < size; j++)
+{
+_putchar(' ');
+}
+for (j = 1; j <= i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - Multiplies two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: Result of the multiplication.
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
